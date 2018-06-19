02f6bd1ab55cbde45d7fc4bbc21a952ed7499747
Alexander Katynia <aleksander.kot@gmail.com>
2018-06-19 11:31:35 +0300

    Refactoring

--- rpn.go
+++ rpn.go
@@ -1,27 +1,33 @@
 package rpn
 
 import (
+	"bytes"
 	"math"
 	"strconv"
 	"strings"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 // FromInfix convert infix expressions to postfix expressions (reverse Polish notation)
 func FromInfix(in string) string {
 	out := newStack()
 	buf := newStack()
+	var tmp bytes.Buffer
 
 	in = strings.Replace(in, " ", "", -1)
 
 	for i := 0; i < len(in); i++ {
 		op := string(in[i])
-		if _, err := strconv.ParseFloat(op, 64); err == nil {
-			out.push(op)
-			continue
-		}
 
 		switch op {
 		case "^", "*", "/", "+", "-":
+			if tmp.Len() > 0 {
+				out.push(tmp.String())
+				tmp.Reset()
+				spew.Dump(out)
+				spew.Dump(tmp)
+			}
 			for {
 				topChar := buf.peak()
 				if topChar == nil {
@@ -48,6 +54,11 @@ func FromInfix(in string) string {
 					break
 				}
 				out.push(l)
+			}
+		default:
+			if _, err := strconv.ParseFloat(op, 64); err == nil {
+				tmp.WriteByte(in[i])
+				continue
 			}
 		}
 	}
