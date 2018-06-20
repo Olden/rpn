6c2eaa1f5737f791216573bbcc8e028d7082cf02
Alexander Katynia <aleksander.kot@gmail.com>
2018-06-20 12:19:53 +0300

    Remove unused code

--- stack.go
+++ stack.go
@@ -1,9 +1,5 @@
 package rpn
 
-import (
-	"strings"
-)
-
 // Stack is a basic LIFO
 type (
 	stack struct {
@@ -14,17 +10,6 @@ type (
 		prev  *node
 	}
 )
-
-// Return string representation of the stack
-func (n *node) string() []string {
-	var out []string
-
-	if n.prev != nil {
-		return append(n.prev.string(), n.value.(string))
-	}
-
-	return append(out, n.value.(string))
-}
 
 // Push adds an element to the stack.
 func (s *stack) push(value interface{}) {
@@ -45,11 +30,6 @@ func (s *stack) peak() interface{} {
 		return nil
 	}
 	return s.top.value
-}
-
-// String returns string representation of stack elements separated by space
-func (s *stack) string() string {
-	return strings.Join(s.top.string(), " ")
 }
 
 // NewStack returns a new stack.
